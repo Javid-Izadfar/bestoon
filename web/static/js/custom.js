b5ac663545eba0c7f07329e0922f300300129050
Javid Izadfar <javid.izadfar@gmail.com>
2017-01-29 14:53:09 +0330

    Added Header

---
+++ web/static/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   function Navbartoggler(){
+      if ($(this).scrollTop() > 1) {
+         $('#header_navbar').addClass('sticky-navbar grey-darken-4 elevate-2').removeClass('super-thick-navbar')
+      } else {
+         $('#header_navbar').removeClass('sticky-navbar grey-darken-4 elevate-2').addClass('super-thick-navbar')
+      }
+   }
+   $(window).scroll(function() {
+      Navbartoggler()
+   })
+   Navbartoggler()
+})
